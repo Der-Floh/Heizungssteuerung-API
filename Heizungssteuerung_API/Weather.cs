bc658035157316b470dedfb3d6409ff09c2d9cc1
Der-Floh <derr.floh@gmail.com>
2023-11-30 07:58:43 +0100

    Projekt Files

---
+++ Heizungssteuerung_API/Weather.cs
@@ -0,0 +1,25 @@
+﻿using Weather.NET.Models.WeatherModel;
+
+namespace Heizungssteuerung_API;
+
+public sealed class Weather
+{
+    public bool Rainy { get => RainVolume > 0; }
+    public double RainVolume { get; set; }
+    public bool Snowy { get => SnowVolume > 0; }
+    public double SnowVolume { get; set; }
+    public bool Cloudy { get => Cloudiness > 0; }
+    public double Cloudiness { get; set; }
+    public double Temperature { get; set; }
+
+    public Weather(WeatherModel? weatherModel = null)
+    {
+        if (weatherModel is not null)
+        {
+            RainVolume = weatherModel.Rain?.Past3HoursVolume ?? 0;
+            SnowVolume = weatherModel.Snow?.Past3HoursVolume ?? 0;
+            Cloudiness = weatherModel.Clouds.Percentage / 100;
+            Temperature = weatherModel.Main.Temperature;
+        }
+    }
+}
---
+++ Heizungssteuerung_API/WeatherAPI.cs
@@ -0,0 +1,64 @@
+﻿using Weather.NET;
+using Weather.NET.Enums;
+using Weather.NET.Models.WeatherModel;
+
+namespace Heizungssteuerung_API;
+
+public sealed class WeatherAPI
+{
+    public string APIKey { get => _apiKey; set { _apiKey = value; _client = new WeatherClient(value); } }
+    private string _apiKey = "6b50f64b6769a340c588baf0eb0e2417";
+    public string City { get; set; } = "Krefeld";
+    private WeatherClient _client;
+
+    public WeatherAPI(string? apiKey = null)
+    {
+        if (!string.IsNullOrEmpty(apiKey))
+            APIKey = apiKey;
+
+        _client = new WeatherClient(APIKey);
+    }
+
+    public async Task<double> GetCurrentTemperature()
+    {
+        WeatherModel weather = await _client.GetCurrentWeatherAsync(City, Measurement.Metric);
+        return weather.Main.Temperature;
+    }
+
+    public async Task<Weather> GetCurrentWeather()
+    {
+        WeatherModel weatherModel = await _client.GetCurrentWeatherAsync(City, Measurement.Metric);
+        Weather weather = new Weather(weatherModel);
+        return weather;
+    }
+
+    public async Task<double[]> GetFutureTemperatures(int forecastCount)
+    {
+        WeatherModel[] weatherData = (await _client.GetForecastAsync(City, forecastCount, Measurement.Metric)).ToArray();
+        double[] temperatures = new double[weatherData.Length];
+        for (int i = 0; i < weatherData.Length; i++)
+            temperatures[i] = weatherData[i].Main.Temperature;
+        return temperatures;
+    }
+
+    public async Task<double[]> GetFutureDaysTemperatures(int dayCount)
+    {
+        int timeStampCount = 24 * dayCount / 3;
+        return await GetFutureTemperatures(timeStampCount);
+    }
+
+    public async Task<Weather[]> GetFutureWeather(int forecastCount)
+    {
+        WeatherModel[] weatherData = (await _client.GetForecastAsync(City, forecastCount, Measurement.Metric)).ToArray();
+        Weather[] temperatures = new Weather[weatherData.Length];
+        for (int i = 0; i < weatherData.Length; i++)
+            temperatures[i] = new Weather(weatherData[i]);
+        return temperatures;
+    }
+
+    public async Task<Weather[]> GetFutureDaysWeather(int dayCount)
+    {
+        int timeStampCount = 24 * dayCount / 3;
+        return await GetFutureWeather(timeStampCount);
+    }
+}
